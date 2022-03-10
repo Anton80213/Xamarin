02d7993c7ab7e0a6365dd27bbc85142c8d65aa1d
Anton80213 <antony.aleev@yandex.ru>
2022-03-10 17:22:03 +0400

    Добавьте файлы проекта.

---
+++ Авторефераты/Авторефераты/Page2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Авторефераты
+{
+    public class Page2: ContentPage 
+    {
+        public Page2(Label header2, ListView listView)
+        {
+           Title = "Cтраница 2";
+           Content = new StackLayout { Children = { header2, listView } };
+           header2.HorizontalTextAlignment = TextAlignment.Center;
+           header2.VerticalTextAlignment = TextAlignment.Center;
+           header2.BackgroundColor = Color.GreenYellow;
+           header2.TextColor = Color.Blue;
+        }       
+    }
+}
---
+++ Авторефераты/Авторефераты/StartPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Авторефераты
+{
+    public partial class StartPage: ContentPage
+    {
+        public StartPage()
+	    {
+	            Title = "Главная страница";
+	            Label header = new Label()
+                {
+                    Text = "Добро пожаловать в каталог авторефератов библиотеки АГУ!",
+                };
+                header.HorizontalTextAlignment = TextAlignment.Center;
+                header.VerticalTextAlignment = TextAlignment.Center;
+                header.BackgroundColor = Color.GreenYellow;
+                header.TextColor = Color.Blue;
+                Button nextButton = new Button
+	            {
+	                Text = "Вперед",
+	                HorizontalOptions = LayoutOptions.Center,
+	                VerticalOptions = LayoutOptions.Center
+	            };
+                nextButton.BackgroundColor = Color.Gold;
+	            nextButton.Clicked += OnButtonClicked;
+	            Content = new StackLayout { Children = { header, nextButton} };
+        }
+        private async void OnButtonClicked(object sender, EventArgs e)
+	    {
+	            await Navigation.PushAsync(new Page1());
+	    }
+
+    }
+}
